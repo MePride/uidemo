c7c518980d599696b3f1a9cbd0672192c823d3b4
pride <mepridee@gmail.com>
2025-01-05 20:09:35 +0800

    Initial Commit

---
+++ uidemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  uidemo
+//
+//  Created by Pride on 2025/1/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
